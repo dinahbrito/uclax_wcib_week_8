fbc0965735a87d20f11a5eaa4148010bb311bed8
dinahbrito <dinahbrito@gmail.com>
2020-11-23 17:56:23 -0800

    apple button jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
 jQuery(function() {
-	
+
+         $("#apple").hover(function(){
+           $(this).css("background-color", "white");
+           $(this).css("color", "#242526");
+           $("#change").attr("src", "images/dark_apple_logo.png");
+           }, function(){
+           $(this).css("background-color", "transparent");
+           $(this).css("color", "white");
+           $("#change").attr("src", "images/apple_logo.png");
+         });
 });
